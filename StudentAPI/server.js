48dd9b11fc1c00afd350409425ffacbe048422f4
azartechnet <azartechnet@gmail.com>
2026-07-01 13:55:21 +0530

    updated

---
+++ StudentAPI/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express=require("express");
+const cors=require("cors");
+
+// const authRoutes=require("./routes/authRoutes");
+// const studentRoutes=require("./routes/studentRoutes");
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api",authRoutes);
+// app.use("/api",studentRoutes);
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server is Running..");
+}
+);
