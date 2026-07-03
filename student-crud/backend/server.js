b16c8918446fc5940bded53653521e1e0f298bb3
azartechnet <azartechnet@gmail.com>
2026-07-03 16:15:48 +0530

    updated

---
+++ student-crud/backend/server.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const cors=require("cors");
+require("dotenv").config();
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("MongoDB connected"))
+.catch((err)=>console.log(err));
+
+// app.use("/api/students",require("./routes/studentRoutes"));
+
+app.get("/",(req,res)=>{
+    res.send("API is running");
+});
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
+});
