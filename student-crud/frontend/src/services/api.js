5fdf9a6cebf80901badb5f5a60aa75440ee5638a
azartechnet <azartechnet@gmail.com>
2026-08-04 14:06:36 +0530

    updated

--- student-crud/frontend/src/services/api.js
+++ student-crud/frontend/src/services/api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL:"http://localhost:5000"
+    // baseURL:"http://localhost:5000"
+
+    baseURL: "https://student-crud-tvu8.onrender.com"
 })
